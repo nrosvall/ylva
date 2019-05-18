7d903d04cd230fc7f58d9c0355ab2ff9cde0fedd
Niko Rosvall <niko@byteptr.com>
2019-05-18 22:17:57 +0300

    Use RAND_bytes for password generation

--- pwd-gen.c
+++ pwd-gen.c
@@ -4,23 +4,22 @@
 
 #define _XOPEN_SOURCE 700
 
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
+#include <openssl/err.h>
+#include <openssl/rand.h>
 #include "utils.h"
-#ifdef __MACH__
-#include <mach/clock.h>
-#include <mach/mach.h>
-#endif
 
 /*Generates random number between 0 and max.
  *Function should generate uniform distribution.
  */
 static unsigned int rand_between(unsigned int min, unsigned int max)
 {
-    int r;
+    uint32_t r;
     const unsigned int range = 1 + max - min;
     const unsigned int buckets = RAND_MAX / range;
     const unsigned int limit = buckets * range;
@@ -31,15 +30,19 @@ static unsigned int rand_between(unsigned int min, unsigned int max)
      */
     do
     {
-        r = rand();
+        if(RAND_bytes((unsigned char *)&r, sizeof r) == 0)
+        {
+            ERR_print_errors_fp(stderr);
+            return -1;
+        }
+
     } while (r >= limit);
 
     return min + (r / buckets);
 }
 
 /* Simply generate secure password
- * and output it to the stdout, use clock_gettime for
- * seeding srand
+ * and output it to the stdout. Uses OpenSSL RAND_bytes.
  *
  * Caller must free the return value.
  */
@@ -54,22 +57,7 @@ char *generate_password(int length)
     "0123456789?)(/%#!?)=";
     unsigned int max;
     unsigned int number;
-    struct timespec tspec;
 
-#ifdef __MACH__
-    /*OS X does not have clock_gettime, use clock_get_time*/
-    clock_serv_t cclock;
-    mach_timespec_t mts;
-    host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &cclock);
-    clock_get_time(cclock, &mts);
-    mach_port_deallocate(mach_task_self(), cclock);
-    tspec.tv_sec = mts.tv_sec;
-    tspec.tv_nsec = mts.tv_nsec;
-#else
-    clock_gettime(CLOCK_MONOTONIC, &tspec);
-#endif
-
-    srand(tspec.tv_nsec);
     max = strlen(alpha) - 1;
     pass = tmalloc((length + 1) * sizeof(char));
 
